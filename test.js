cbce035eabb7ea7f9b7204046d08cf24263664a9
Mac <enkindled@pm.me>
2019-08-09 01:34:55 -0700

    Initial design

--- test.js
+++ test.js
@@ -1,6 +1,32 @@
 import test from 'ava';
-import func from '.';
+import {listen, connect} from '.';
 
-test('foo', t => {
-	t.is(func(), true);
+test('client awaits server function', async t => {
+	t.plan(4);
+
+	// Server side
+
+	const serverHandlers = {
+		five: async n => {
+			t.is(n, 3);
+			await new Promise(resolve => setImmediate(resolve));
+			return 5 - n;
+		}
+	};
+
+	const {done: doneServer} = await listen(serverHandlers, {}, socket => {
+		t.pass();
+		socket.on('end', () => t.pass());
+	});
+
+	// Client side
+
+	const {send, done: doneClient} = await connect();
+	const result = await send('five', 3);
+	t.is(result, 2);
+
+	// Cleanup
+
+	await doneClient();
+	await doneServer();
 });
